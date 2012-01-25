2d78ffadaacf47675b5dcb0d9683a1cedf75f83f
Rafael Franco <paxthor@gmail.com>
2012-01-25 10:32:56 -0300

    Primer commit, configuraciones iniciales del proyecto.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gexip::Application.config.secret_token = '4cc4fdcba03c67a2a958ce79f707d98288bfb076026fd458b14dfb739e2ee6b80682db0c7ecd970f145c3c439798dfcf5f9322b3410b043c75ba6aa8bb83d7a9'
