5a8e6e25c3c18571cc5e3cb78e81a838c4d79197
Rafael Franco <paxthor@gmail.com>
2012-02-02 20:27:04 -0300

    Implementaciónb de helper para iconos de bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  # Permite crear un icono para bootstrap con un nombre dado
+  # 
+  # Ejemplo:
+  #  icon_tag('Editar', :class => 'icon-pencil')
+  def icon_tag(name, options = {})
+    options[:class] ||= 'icon-list-alt'
+    content_tag(:i, nil, :class => options[:class]).concat(name)
+  end
 end
