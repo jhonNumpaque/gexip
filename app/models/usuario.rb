a963edfb31f2d77cd95d4d8c2fba5368a40f39fb
Rafael Franco <paxthor@gmail.com>
2012-02-01 21:21:29 -0300

    Modificaciones de estilo en el index. Se adecua al nuevo bootstrap

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,8 +2,15 @@ class Usuario < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable  
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :nombres, :apellidos, :documento, :ente_id, :rol_id, :login
+
+  # validaciones
+  validates :login, :presence => true
+  validates :nombres, :presence => true
+  validates :apellidos, :presence => true
+  validates :documento, :presence => true
+  #validates :documento, :presence => true
 end
