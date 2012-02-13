503e27729812f9332eac124ce42cd8700279bfe6
progresivjose <jmanuelgricart@gmail.com>
2012-02-13 20:54:41 -0300

    cambios en el controlador de cargos y los features

--- features/step_definitions/cargos_steps.rb
+++ features/step_definitions/cargos_steps.rb
@@ -1,13 +1,23 @@
 #encoding: utf-8
 
 def cargo_valido
-	@rol ||= {:nombre=>'recepcionista'}
+	@cargol ||= {:nombre=>'recepcionista'}
 end
 
-def crear_cargo(rol)
+def crear_cargo(cargo)
 	visit '/cargos/new'
-	fill_in 'Nombre', :with => rol[:nombre]
+	fill_in 'Nombre', :with => cargo[:nombre]
 	click_button 'Guardar'
+end
+
+def editar_cargo(cargo)
+	visit '/cargos/edit'
+	fill_in 'Nombre', :with => cargo[:nombre]
+	click_button 'Guardar'
+end
+
+def eliminar_cargo(cargo)
+	cargo.destroy
 end
 
 def iniciar_sesion usuario
@@ -38,3 +48,33 @@ end
 Then /^debería ver un mensaje de nombre requerido$/  do
 	page.should have_content 'Nombreno puede estar en blanco'
 end
+
+When /^edito un cargo con los datos válidos$/ do
+	editar_cargo cargo_valido
+end
+
+Then /^debería ver un mensaje de cargo actualizado$/  do
+	page.should have_content 'Cargo actualizado'
+end
+
+When /^edito un cargo sin especificar el nombre$/ do
+	cargo = cargo_valido
+	cargo[:nombre] = nil
+	editar_cargo cargo
+end
+
+Then /^debería ver un mensaje de nombre requerido$/  do
+	page.should have_content 'Nombreno puede estar en blanco'
+end
+
+#When /^doy click al boton eliminar$/ do
+#	cargo = cargo_valido
+#end
+#
+#And /^confirmo accion$/ do
+#	eliminar_cargo cargo_valido
+#end
+#
+#Then /^debería ver un mensaje de cargo eliminado$/  do
+#	page.should have_content 'Cargo eliminado'
+#end
