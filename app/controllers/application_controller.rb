57939ae99e827704b7a294604acd8ddce7a0a656
Rafael Franco <paxthor@gmail.com>
2012-02-18 13:59:03 -0300

    comentado de chequeo de accesos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  include ControlarAcceso
+  #include ControlarAcceso
   
-  before_filter :controlar_acceso!  
+  #before_filter :controlar_acceso!  
   
   def after_sign_out_path_for(resource_or_scope)    
     login_path
