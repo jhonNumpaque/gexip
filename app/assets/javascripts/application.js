117b4ce986d61c59040998207bd49e519ad0e4a3
Rafael Franco <paxthor@gmail.com>
2012-02-18 13:56:04 -0300

    agrego métodos para marcar y desmarcar checkboxs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+function markAll() {  
+  $("input[type='checkbox']").attr('checked', true);      
+}
+
+function unmarkAll() {    
+  $("input[type='checkbox']").attr('checked', false);    
+}
