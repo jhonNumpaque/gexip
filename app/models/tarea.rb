9ea47256ed248c65bb1539217a9a616158eead4d
Diego <diegokc@msn.com>
2012-03-12 19:45:46 -0300

    commit de tarea para unir con el github

--- app/models/tarea.rb
+++ app/models/tarea.rb
@@ -1,12 +1,13 @@
 class Tarea < ActiveRecord::Base
   #validaciones
-  validates :orden, :presence => true
+# validates :orden, :presence => true
   validates :nombre, :presence => true
   validates :tipo, :presence => true
   validates :cargo_id, :presence => true
   
   #Relaciones
   belongs_to :cargo, :foreign_key => :cargo_id
+  belongs_to :actividad, :foreign_key => :actividad_id
 
   # CONSTANTE
   TIPO_TAREA = %w{INICIO PROCESO TRANSLADO ALMACENAMIENTO LOGICA FIN FIN_ALTERNANTIVO}
