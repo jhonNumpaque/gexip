adcb9b0f544bc8b5a3552b1c05bcbc4f2d2be725
progresivjose <jmanuelgricart@gmail.com>
2012-03-12 21:04:38 -0300

    cambios en tareas

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -7,6 +7,7 @@ class TareasController < ApplicationController
     respond_to do |format|
       format.html { render :layout => "popup_wf" } # index.html.erb
       format.json { render json: @tareas }
+	  format.js
     end
   end
 
@@ -29,6 +30,7 @@ class TareasController < ApplicationController
     respond_to do |format|
       format.html { render :layout => "popup_wf" } # new.html.erb
       format.json { render json: @tarea }
+	  format.js
     end
   end
 
@@ -39,7 +41,7 @@ class TareasController < ApplicationController
 		respond_to do |format|
 			#      format.html { render :layout => "popup_wf" } # new.html.erb
 			#      format.json { render json: @tarea }
-      format.js 
+			format.js 
     end
   end
 
@@ -50,10 +52,10 @@ class TareasController < ApplicationController
 
     respond_to do |format|
       if tarea.save
-				@tareas = Tarea.where(:actividad_id => tarea.actividad_id).order("orden")
+		@tareas = Tarea.where(:actividad_id => tarea.actividad_id).order("orden")
         format.html { redirect_to tareas_path(:actividad_id => @tarea.actividad_id), notice: 'Tarea Creada Correctamente.' }
         format.json { render json: @tarea, status: :created, location: @tarea }
-				format.js
+	    format.js
       else
         format.html { render action: "new", :layout => "popup_wf" }
         format.json { render json: @tarea.errors, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -34,9 +34,9 @@ Gexip::Application.routes.draw do
 
   resources :usuarios
 	
-  match '/:id/agregar_tarea' => 'actividades#agregar_tarea', :as => 'agregar_tarea'
-  match '/:id/listar_tareas' => 'actividades#listar_tareas', :as => 'listar_tareas'
-  match '/editar_tarea' => 'tareas#edit', :as => 'editar_tarea'
+#  match '/:actividad_id/agregar_tarea' => 'tareas#new', :as => 'agregar_tarea'
+#  match '/:actividad_id/listar_tareas' => 'tareas#index', :as => 'listar_tareas'
+#  match '/editar_tarea' => 'tareas#edit', :as => 'editar_tarea'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
