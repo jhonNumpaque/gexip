e6fd536440523d93fef3141e529ebc85105754c1
Rafael Franco <paxthor@gmail.com>
2012-04-16 20:37:37 -0400

    Cambios en expedientes para establecrer valores predeterminados en número y estado

--- app/models/expediente.rb
+++ app/models/expediente.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class Expediente < ActiveRecord::Base
   #validaciones
   validates :procedimiento_id, :presence => true
@@ -7,7 +8,8 @@ class Expediente < ActiveRecord::Base
   validates :estado, :presence => true
   validates :usuario_ingreso_id, :presence => true
   validates :fecha_ingreso, :presence => true
-  validates :numero, :presence => true
+  validates :numero, :presence => true, :uniqueness => true
+  #validates :numero_documento, :presence => true
       
   #Relaciones
   belongs_to :procedimiento, :foreign_key => :procedimiento_id
@@ -22,5 +24,40 @@ class Expediente < ActiveRecord::Base
   ESTADO = %w{NUEVO RECIBIDO TRANSITO RECHAZADO FINALIZADO ANULADO PROCESANDO}
   
   attr_accessor :numero_documento
+  
+  # callbacks
+  before_validation :generar_numero, :establecer_estado
+  
+  def generar_numero
+    return true unless self.new_record?
+    
+    anyo = Date.today.year
+    ultimo_expediente = Expediente.last
+    
+    ultimo_anyo = ultimo_expediente.anyo_ingreso if ultimo_expediente.present?
+    
+    if ultimo_expediente.blank? || (ultimo_anyo.to_s != anyo.to_s)
+      numero = "#{anyo}#{1}"
+    else
+      numero = "#{anyo}#{ultimo_expediente.siguiente_numero}"
+    end
+    
+    self.numero = numero
+  end
+  
+  def anyo_ingreso
+    self.numero.to_s[0..3]
+  end
+  
+  def siguiente_numero
+    n = self.numero.to_s
+    n[4..n.length].next
+  end
+  
+  def establecer_estado
+    return true unless self.new_record?
+    
+    self.estado = ESTADO[0]
+  end
   
 end
