aa1d55619e9d074323d08c75286c30ca1f3c97be
Jose Manuel <jmanuelgricart@gmail.com>
2013-08-16 20:33:17 -0400

    se agrega la funcion para calcular la fecha aproximada de entrega del expediente

--- app/models/procedimiento.rb
+++ app/models/procedimiento.rb
@@ -26,23 +26,31 @@ class Procedimiento < ActiveRecord::Base
   def self.calcular_fecha_entrega(tiempo)
     if tiempo > 0
       tiempo_actual = DateTime.now
+      puts tiempo_actual.wday
       #verificar si la consulta se realiza en horas laborales
-      if tiempo_actual.wday != 6 || tiempo_actual.wday != 6
+      if ![6,7].include? tiempo_actual.wday
         if (Estructura::Hora_Trabajo_Inicio..Estructura::Hora_Trabajo_Fin).to_a.index(tiempo_actual.hour).present?
           horas_de_trabajo = (Estructura::Hora_Trabajo_Inicio..Estructura::Hora_Trabajo_Fin).to_a.index(12)
           minutos_de_trabajo = horas_de_trabajo * 60 #pasar las horas de trabajo a minutos trabajados para normalizar
           if (tiempo - minutos_de_trabajo) > 0
             tiempo = tiempo - minutos_de_trabajo #se resta el tiempo que se trabajara ese dia
           end
-          dias_faltantes = tiempo / Estructura::Minutos_Dias #se vuelve a calcular el aproximado de dias
+          dias_faltantes = tiempo.to_f / Estructura::Minutos_Dias.to_f #se vuelve a calcular el aproximado de dias
         else
-
           if tiempo_actual.hour < 7 #verificar si se pregunta en horas de la madrugada
             dias_faltantes += tiempo.to_f / Estructura::Minutos_Dias.to_f #se vuelve a calcular el aproximado de dias
           elsif tiempo_actual.hour > 15 #verificar si se pregunta en horas de la tarde
             dias_faltantes = 1.00
             dias_faltantes += tiempo.to_f / Estructura::Minutos_Dias.to_f #se vuelve a calcular el aproximado de dias
           end
+        end
+      else
+        #En caso que la consulta se haga un sabado o un domingo
+        dias_faltantes = tiempo.to_f / Estructura::Minutos_Dias.to_f #se vuelve a calcular el aproximado de dias
+        if tiempo_actual.wday == 6
+          dias_faltantes += 2.0
+        elsif tiempo_actual.wday == 7
+          dias_faltantes += 1.0
         end
       end
       #sumar la cantidad de dias, agregando los sabados y domingo
@@ -55,9 +63,14 @@ class Procedimiento < ActiveRecord::Base
       end
       dias_faltantes = dias_faltantes + cant_dias # se suma la cantidad de sabados y domingos
       minutos_faltantes = dias_faltantes.to_s.split('.')[1] #obtener los minutos restantes
-      minutos_faltantes = '0.' + minutos_faltantes
+      minutos_faltantes = '0.' + minutos_faltantes.to_s
       minutos_faltantes = minutos_faltantes.to_f
       minutos_faltantes *= Estructura::Minutos_Dias
+      if minutos_de_trabajo.present?
+        if (minutos_faltantes + minutos_de_trabajo) < Estructura::Minutos_Dias
+          minutos_faltantes += minutos_de_trabajo #se agrega la cantidad de horas que ya se trabajo en el dia
+        end
+      end
       fecha_estimada = tiempo_actual + dias_faltantes.days #calcular la cantidad de dias
       fecha_entrega = DateTime.new(fecha_estimada.year, fecha_estimada.month, fecha_estimada.day, Estructura::Hora_Trabajo_Inicio) #se crea la nueva fecha con el tiempo a las 7am
       fecha_entrega = fecha_entrega + minutos_faltantes.minute #se suman los minutos faltantes para la entrega
