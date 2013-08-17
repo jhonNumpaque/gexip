c3338373d05fa467eb3cfaf6fada402de6c2f934
Diego <diegokc@msn.com>
2013-08-16 21:42:18 -0400

    tiempos demorados 2

--- app/models/tiempo_demorado.rb
+++ app/models/tiempo_demorado.rb
@@ -40,4 +40,58 @@ class TiempoDemorado
     end
   end
 
+  class << self
+    def f_reporte_603(fecha1,fecha2,pi_funcionario_id)
+      result = ActiveRecord::Base.connection.execute(" SELECT * FROM f_reporte_603('#{fecha1}','#{fecha2}','#{pi_funcionario_id}') ")
+
+      if result.count == 0
+        #return [TiempoDemorado.new(0, "N/A", "N/A")]
+        return nil
+      end
+
+        result.map { |r| TiempoDemorado.new(r["cid"], r["cnumero"], r["cfecha_inicio"], r["cminutos_demorados"], r["cminutos_estimados"], r["cminutos"], r["cfuncionario"]) }
+    end
+  end
+
+  class << self
+    def f_reporte_604(fecha1,fecha2,procedimineto_id)
+      #result = ActiveRecord::Base.connection.execute("SELECT * FROM stock_hourly_performance(#{stock_id})")
+      #RETURNS TABLE(ctiempo double precision, cnombres character varying, capellidos character varying) AS
+      result = ActiveRecord::Base.connection.execute(" SELECT * FROM f_reporte_604('#{fecha1}','#{fecha2}','#{procedimineto_id}') ")
+
+      if result.count == 0
+        #return [TiempoDemorado.new(0, "N/A", "N/A")]
+        return nil
+      end
+
+        result.map { |r| TiempoDemorado.new(r["cid"], r["cnumero"], r["cfecha_inicio"], r["cminutos_demorados"], r["cminutos_estimados"], r["cminutos"], r["cfuncionario"]) }
+    end
+  end
+  
+  class << self
+    def f_reporte_605(fecha1,fecha2,cargo_id)
+      result = ActiveRecord::Base.connection.execute(" SELECT * FROM f_reporte_605('#{fecha1}','#{fecha2}','#{cargo_id}') ")
+
+      if result.count == 0
+        #return [TiempoDemorado.new(0, "N/A", "N/A")]
+        return nil
+      end
+
+        result.map { |r| TiempoDemorado.new(r["cid"], r["cnumero"], r["cfecha_inicio"], r["cminutos_demorados"], r["cminutos_estimados"], r["cminutos"], r["cfuncionario"]) }
+    end
+  end
+
+  class << self
+    def f_reporte_606(fecha1,fecha2,pi_funcionario_id)
+      result = ActiveRecord::Base.connection.execute(" SELECT * FROM f_reporte_606('#{fecha1}','#{fecha2}','#{pi_funcionario_id}') ")
+
+      if result.count == 0
+        #return [TiempoDemorado.new(0, "N/A", "N/A")]
+        return nil
+      end
+
+        result.map { |r| TiempoDemorado.new(r["cid"], r["cnumero"], r["cfecha_inicio"], r["cminutos_demorados"], r["cminutos_estimados"], r["cminutos"], r["cfuncionario"]) }
+    end
+  end
+
 end
